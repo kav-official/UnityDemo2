779e37ea49ef8642f0f233edd66b155011a61efb
kav-oficial <katrade1997@gmail.com>
2025-05-11 19:21:35 +0700

    ka uupdated

---
+++ myApp/EMplayee.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPtutorial
+{
+        class Employee 
+        {
+            public string name = "a ka";
+            public int salary = 100;
+
+            // static void Main(string[] args){
+
+            // }
+ 
+            // public Employee(string nameValue)
+            // {
+            //     Console.Write("Create object from employe class");
+            // }
+            // public Employee(string nameValue)
+            // {
+
+            // }
+        }
+}
---
+++ myApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using  System;
+
+class Program {
+    static void Main(string[] args)
+    {
+
+        Employee emp = new Employee();
+        Console.WriteLine(emp.name);
+
+
+
+
+
+
+
+
+
+
+        // string myString = "200.15";
+        // byte myByte = (byte)Convert.ToSByte(myString);
+        // int myInt = Convert.ToInt32(myString);
+        // byte myByte = Byte.Parse(myString);
+
+        // float myInt;
+        // bool success = float.TryParse(myString,out myInt);
+
+        // if(!string.IsNullOrEmpty(myString)){
+        //     Console.WriteLine("string is not null");
+        // }else{
+        // Console.WriteLine("string is null");                   
+        // }
+
+        // double x = 100.24;
+        // int y = (int)x;
+        // float x = 100f;
+        // double y = x;
+
+        // double weight, height; 
+         
+        // Console.Write("input weight: (kg)");
+        // double.TryParse(Console.ReadLine(),out weight);
+        // Console.Write("input Height (m)");
+        // double.TryParse(Console.ReadLine(),out height);
+        // height = height/100;
+        // double ibm = weight/Math.Pow(height,2);
+        // Console.WriteLine("Your IBM is: {0:F2}"+ibm);
+        // Console.WriteLine(myInt);
+
+        // Console.Write(emp);
+        // Console.ReadKey();
+    }
+}
